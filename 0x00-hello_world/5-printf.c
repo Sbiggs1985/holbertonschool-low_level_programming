ba6b9d7a9b08606e12ff3b08c5ba93518043fb76
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-26 08:55:33 -0800

    Puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Hello, printf task*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
