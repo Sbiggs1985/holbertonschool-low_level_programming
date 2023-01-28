389c46211d4b84d38250bdf18f0888bc72e7a70b
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-28 12:07:17 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+ * main - Time to run
+ *
+ * Return: Or time to turn
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d", c);
+	}
+	putchar ('\n');
+	return (0);
+}
