aa514e614e290191fa9adec5217eea2ca5665e08
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-28 12:24:11 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry
+ *
+ * Return: Hexadecimal
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+	char a;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar ('\n');
+	return (0);
+}
