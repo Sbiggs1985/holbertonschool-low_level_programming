da02bb7b5e4edbc4ba86d1e418e7cb57dd45ed86
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-28 12:14:34 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry
+ *
+ * Return: around
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+	}
+	putchar ('\n');
+	return (0);
+}
