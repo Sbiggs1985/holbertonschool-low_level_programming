9f50fecce88e32f70a207508e9b65240e5af2573
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-28 12:27:19 -0800

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry
+ *
+ * Return: patience
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
