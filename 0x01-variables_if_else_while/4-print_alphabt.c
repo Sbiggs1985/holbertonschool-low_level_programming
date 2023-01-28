27c9af9fb9d75fd0de6bfbbce422c132b401b8d7
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-28 11:54:29 -0800

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+/**
+ * *main - jump
+ * *Return: around
+ * */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
