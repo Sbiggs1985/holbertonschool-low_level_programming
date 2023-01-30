32e70282a67ca8fcffed6b54bc3c3407cc6ed84a
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-29 16:53:05 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry
+ *
+ * Return: putchar
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+
+	putchar(ch);
+}
+	return (0);
+}
