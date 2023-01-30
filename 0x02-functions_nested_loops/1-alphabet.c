6f706dbee5a5dfd456f86843c82364a339816a28
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-30 10:48:01 -0800

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
+/**
+* main - Entry
+* Return: im dieing LOL
+*
+*/
 
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
