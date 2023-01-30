647287a63767b11af9245051b198d687efbad5a1
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-01-30 08:57:48 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
 /**
- * main - Entry
+ *main - Entry point
  *
- * Return: putchar
+ *Return: This makes no sense to me
  */
-
 #include "main.h"
 
 int main(void)
+{
+	char mpc[] = "_putchar";
+
+	int i;
+
+	for (i = 0; mpc[i]; i++)
+	{
+		_putchar(mpc[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
