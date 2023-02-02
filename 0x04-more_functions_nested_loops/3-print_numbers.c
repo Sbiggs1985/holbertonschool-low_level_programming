4566532d58d31c7a5fb9628a6d477e8cfef77509
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-02 11:39:33 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,16 +5,13 @@
 * Description: We are using the syntax to print the numbbers
 * Return: We will return nothing
 */
-
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-
 	{
-	_putchar(i + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
-	return (0);
 }
