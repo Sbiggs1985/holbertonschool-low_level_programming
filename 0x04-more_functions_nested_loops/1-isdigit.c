7bfaf2e512e807b6c0668e9b64b57b843eeabf05
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-02 10:13:36 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+ /**
+ * _isdigit - Return 1 if c is uppercase
+ * @c: we have declared the char c
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
