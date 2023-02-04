9440dcf5323347bed7e13015cb09405e4377ef90
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-04 13:11:38 -0800

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+/**
+ * main entry - The fixx buzz
+ * *Return: nothing
+ */
+#include <stdio.h>
+
+int main(void)
+{
+
+	int n = 1;
+
+	for (; n <= 99; n++)
+	{
+		if (n % 15 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else if (n % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (n % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else
+	{
+		printf("%d ", n);
+	}
+		}
+		printf("Buzz");
+		printf("\n");
+			return (0);
+}
