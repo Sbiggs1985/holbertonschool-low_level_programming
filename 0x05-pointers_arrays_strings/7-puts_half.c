b3913ed88cdfa199d872a8fd4300b4cd4acd512c
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-06 11:15:58 -0800

    problem7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,32 @@
 #include "main.h"
 /**
- *
- *
+ * puts_half - Entry Point
+ * @str: get string
+ * Return: Return half
  */
+
 void puts_half(char *str);
+{
+	int length = 0;
+	int n;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 != 0)
+	{
+		n = ((length + 1) / 2);
+	}
+	else
+	{
+		n = (length / 2)
+	}
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		x++;
+	}
+	_putchar('\n');
+}
+
