a13d39ed3a5f035274ae23d03dc3b8722dbe0335
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-05 18:35:52 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+/**
+ * print_square - Print a square
+ * @size: use to print a square
+ * Return: Always 0
+ */
+void print_square(int size);
+{
+	char square = '#';
+	int x1;
+	int x2;
+
+	if (size > 0)
+	{
+
+		for (x1 = 0; x1 < size; x1++)
+		{
+
+		for (x2 = 0; x2 < size; x2++)
+		{
+			_putchar(square);
+		}
+			_putchar('\n');
+			}
+		}
+	else
+	{
+	_putchar('\n');
+	}
+}
