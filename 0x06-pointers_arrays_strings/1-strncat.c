4583ba16922553306204d41a2c398400036744fd
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-09 07:32:46 -0800

    problem1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,24 @@
 #include "holberton.h"
 /**
- *
- *
+ * _strncat - A function that concatenates two strings
+ * @dest: An argument
+ * @src: An argument
+ * Returns: Always 0
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, y;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (y = 0; y < n; y++)
+	{
+		dest[i + y] = src[y];
+		if (src[y] == '\0')
+			y = n;
+	}
+	return (dest);
+}
+
