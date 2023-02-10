72457633bed2fa55b11b30a658640fb972534be5
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-10 07:39:40 -0800

    problem5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * string_toupper - A function that changes lower of a string to upper
+ * @n: Input used to return n
+ * Return: capitalized string
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
+	int i;
+
+	for (i = 0; n[i] != '\0'; itt)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
