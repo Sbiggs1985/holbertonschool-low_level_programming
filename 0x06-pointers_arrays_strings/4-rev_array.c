72a188d406fef86cf932ffff89d48c01d008eb3f
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-10 07:31:13 -0800

    problem4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
+ * reverse_array - A function that resverses
+ * @a: content
+ * @n: integer
+ * Return: Always 0 (yay)
  */
 void reverse_array(int *a, int n)
 {
+	int i, temp;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
