430bb1bfc874b45a60d6ea2440845250f4fec0ad
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-13 17:41:33 -0800

    problem3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,13 +7,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-
+i
 	unsigned int n = 0;
 	int r;
 
 	while (*s)
 	{
-		for (r - 0; accept[r]; r++)
+		for (r = 0; accept[r]; r++)
 		{
 			if (*s == accept[r])
 			{
