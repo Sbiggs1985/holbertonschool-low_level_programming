cf02071202cdcc319dfade6f2831d312a5d07f49
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-16 09:31:07 -0800

    yo

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,11 @@
 #include "main.h"
-int prime_checker(int n, int i);
-/**
- * _sqrt_recursion - No loop, 1, function
- * @n: Input
- * @i: Input
- * Return: Always 0
- */
-int _sqrt_recursion(int n)
-{
-	if (n <= 1)
-		returrn (0);
-	else if (prime_checker(n, n / 2) > 0)
-		return (1);
-	return (0);
-}
 
 /**
- *
- *
- *
+ * _sqrt_recursion -
+ * 
+ * 
+ * Return: Always 0
  */
-int -prime_checker(int n, int i)
+int _sqt_recursion(int n)
 {
-	if (i == 1)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	else
-		return (prime_checker(n, i - 1));
-}
+
