3736a6510c68fe491c41ac58448e77b5514c1a85
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-24 18:13:54 -0800

    problem3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,4 +3,42 @@
 #include <stdlib.h>
 
 /**
- * 
+ * alloc_grid - nested loop
+ * @width: width
+ * @height: height
+ * Return: Always 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **iarray;
+	int i, n;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	iarray = malloc(sizeof(int) * height);
+
+		if (iarray == NULL)
+
+	{
+			free(iarray);
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		iarray[i] = malloc(sizeof(int) * width);
+		if (iarray == NULL)
+	{
+			free(iarray);
+			return (NULL);
+	}
+
+		for (n = 0; n < width; n++)
+		{
+			iarray[i][n] = 0;
+		}
+	}
+	return (iarray);
+}
