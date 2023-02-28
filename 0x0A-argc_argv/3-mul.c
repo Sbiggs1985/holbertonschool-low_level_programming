f2efd04b1fe48f1132dab279f3fcf9bd39052312
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-02-28 08:57:00 -0800

    problem3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * @argc: input
@@ -12,14 +13,16 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		n1 = atoi(argv[1];
+		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
+	
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	
 	return (0);
 }
