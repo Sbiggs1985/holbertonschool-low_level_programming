d4cc6d1691d44cf7b914adb2ac44066b063dd8f8
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-02 12:18:00 -0800

    problem0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
