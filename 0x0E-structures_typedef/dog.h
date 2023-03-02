014072caf2082b99261a4730814d1405ddb7f966
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-02 12:30:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,5 @@ i	char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 my_dog *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void free_dog(my_dog *d);
 #endif
