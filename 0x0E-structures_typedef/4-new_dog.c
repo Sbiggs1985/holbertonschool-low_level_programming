6ace0fc43c2f5dc165297b59164c890bc9c13c9a
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-06 10:37:51 -0800

    problem4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+
+/**
+ * new_dog - New dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p_dog;
+
+	int ln, lo, i;
+
+	p_dog = malloc(sizeof(*p_dog));
+
+	if (p_dog == NULL || !(name) || !(owner))
+	{
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (ln = 0; name[ln]; ln++)
+		;
+
+	for (lo = 0; owner[lo]; lo++)
+		;
+	p_dog->name = malloc(ln + 1);
+	p_dog->owner = malloc(lo + l);
+
+	if ((p_dog->name) == NULL || (p_dog->) == NULL)
+	{
+		free(p_dog->owner);
+		free(p_dog->name);
+		free(p_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < ln; i++)
+		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
+
+	p_dog->age = age;
+
+	for (i = 0; i < lo; i++)
+		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
+
+	return (p_dog);
+}
