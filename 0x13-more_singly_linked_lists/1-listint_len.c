4146bea648134da85dbaa4d8e354230561b050e5
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-16 20:34:35 -0700

    problem1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number
+ * @h: Input
+ * Return: 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	int len = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
