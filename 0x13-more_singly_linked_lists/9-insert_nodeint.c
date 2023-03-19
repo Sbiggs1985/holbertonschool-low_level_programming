578c7ee6226f018827a01242b5340f6d9630d0a2
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-18 21:35:07 -0700

    problem9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,8 +27,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_mode->n = n;
-	new_mode->next = current;
+	new_node->n = n;
+	new_node->next = current;
 
 	if (prev == NULL)
 		*head = new_node;
