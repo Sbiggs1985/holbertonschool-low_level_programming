99f53141a6901ab6001fb8f87c986ddfae50ac2c
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-19 17:16:36 -0700

    problem0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,15 @@ unsigned int binary_to_uint(const char *b)
 
 	size_t len = strlen(b);
 	unsigned int num = 0;
+	unsigned int base = 1;
 
-	for (size_t i = 0; i < len; i++)
+	for (int i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
-		return (0);
-	num += (b[i] - '0') * pow(2, len - 1 - 1);
+			return (0);
+
+		num += (b[i] - '0') * base;
+		base *= 2;
 	}
 
 	return (num);
