405e676bb411085b56a44f81ed836df964725071
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-20 16:31:42 -0700

    problem3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * set_bit - Entry Point
- * @n: Input
- * @index: Index
- * Return: 0.
+ * set_bit - Write a function that sets the value. 
+ * @n: *n is a pointer
+ * @index: Index is the Index.
+ * Return: -1 or maybe 0. 
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
