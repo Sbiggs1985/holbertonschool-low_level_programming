c6a1c9856507c32d48546c7bc60c93f708d1c4d0
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-20 19:57:45 -0700

    problem5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,8 +5,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (!(head))
-		return;
-	free_listint(*head);
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
 	*head = NULL;
 }
