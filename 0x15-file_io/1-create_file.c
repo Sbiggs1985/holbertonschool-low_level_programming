fa6cb4ab77587191dc6ecc44f0a30f0bf3b75de5
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-20 19:38:24 -0700

    problem1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,27 @@ int create_file(const char *filename, char *text_content)
 {
 	int file, i = 0;
 
-	if (file == NULL)
+	ssize_t len;
+
+	if (filename == NULL)
 		return (-1);
+
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (file == -1)
 		return (-1);
 
-	while (text_control[i])
-		i++;
-
-	if (text_control == NULL)
+	if (text_content == NULL)
 	{
 		close(file);
-		return (-1);
+		return (-2);
 	}
 	else
 	{
-		write(file, text_content, i);
+		len = strlen(text_content);
+		write(file, text_content, len);
 	}
 
 	close(file);
-	return (1);
+	return (-1);
 }
