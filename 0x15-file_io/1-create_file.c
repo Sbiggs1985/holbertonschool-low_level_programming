c62a0193846630ab2777afb55d04a14365fb8368
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-21 12:45:37 -0700

    problem21

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,18 +12,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int err, len, fd;
 
-	fd = open(filename, Oi_CREAT | O_WRONLY | O_TRUNC, 0600);
+	err = len = fd = 0;
 
-	if (fd == -1)
+	if (!filename)
 		return (-1);
 
-	if (text_content)
-	{
-		if (write(fd, text_content, strlen(text_content)) == -1)
-			return (-1);
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd < 0)
+		return (-1);
+
+	while (text_content && text_content[len])
+		len++;
+
+	err = write(fd, text_content, len);
+
+	if (err < 0)
+		return (-1);
 	close(fd);
-	return (-1);
+	return (1);
 }
