9a7f717cce1b3f0713aa8309602e66f71728019d
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-24 16:46:23 -0700

    checking task 0

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,19 @@
 /**
  * print_dlistint - A function that prints all the elements of dlistint
  * @h: Pointer to first node
- * 
+ *
  * Return: Number of nodes
- */ 
+ */
+size_t print_dlistint(cont dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
