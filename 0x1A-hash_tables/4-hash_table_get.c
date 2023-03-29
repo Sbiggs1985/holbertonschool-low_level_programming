8fd9fb96fafe9afa1d61ecd36e52824c2dc0a9f9
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-28 17:43:54 -0700

    task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Get a value
- * @ht: Hash table
- * @key: Key
- * Return: A value
+ * hash_table_get - Getting a hash table
+ * @ht: Pointer to the ht
+ * @key: Really depends on the lock LOL
+ * Return: The value associated with the element.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
