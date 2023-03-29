444209bd5fc093652850fac3479d5a3a374fa8ff
Sbiggs1985 <biggs.stephaine@yahoo.com>
2023-03-28 17:49:00 -0700

    task0

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,24 +9,23 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
-	unsigned long int index;
+	unsigned long int index = 0;
+	hash_node_t *temporal = NULL;
 
-	if (!ht || !key || *key == '\0')
+	if (!key)
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	if (ht)
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+		temporal = ht->array[index];
 
-	if (index >= ht->size)
-		return (NULL);
-
-	node = ht->array[index];
-
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
-	if (node == NULL)
-		return (NULL);
-	else
-		return (node->value);
+	while (temporal)
+	{
+		if (strcmp(temporal->key, key) == 0)
+			return (temporal->value);
+		temporal = temporal->next;
+		}
+	}
+	return (NULL);
 }
